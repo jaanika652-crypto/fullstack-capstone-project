dc9ea34a8693e4045fe480e6a8f08da540d3899a
jaanika652-crypto <jaanika652@gmail.com>
2025-12-29 08:27:51 -0500

    Muuutused

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -3,45 +3,44 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
+const pinoHttp = require('pino-http');
 
 const connectToDatabase = require('./models/db');
-const {loadData} = require("./util/import-mongo/index");
-
-
-const app = express();
-app.use("*",cors());
-const port = 3060;
-
-// Connect to MongoDB; we just do this one time
-connectToDatabase().then(() => {
-    pinoLogger.info('Connected to DB');
-})
-    .catch((e) => console.error('Failed to connect to DB', e));
-
-
-app.use(express.json());
+const { loadData } = require('./util/import-mongo/index');
 
 // Route files
 const giftRoutes = require('./routes/giftRoutes');
 const searchRoutes = require('./routes/searchRoutes');
-const pinoHttp = require('pino-http');
-const logger = require('./logger');
 
-app.use(pinoHttp({ logger }));
+const app = express();
+const port = 3060;
+
+// Middleware
+app.use("*", cors());
+app.use(express.json());
+app.use(pinoHttp({ logger: pinoLogger }));
+
+// Connect to MongoDB (once)
+connectToDatabase()
+    .then(() => {
+        pinoLogger.info('Connected to DB');
+    })
+    .catch((e) => console.error('Failed to connect to DB', e));
 
 // Use Routes
-app.use('/api/gifts', giftRoutes);
-app.use('/api/search', searchRoutes);
+app.use('/api/gifts', giftRoutes);     // Gift-related endpoints
+app.use('/api/search', searchRoutes);  // Search endpoints
+
+// Example route
+app.get("/", (req, res) => {
+    res.send("Inside the server");
+});
 
 // Global Error Handler
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).send('Internal Server Error');
 });
-
-app.get("/",(req,res)=>{
-    res.send("Inside the server")
-})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
